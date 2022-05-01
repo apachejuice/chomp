17f718d1e0df0eaac21029396cd174d212e8d2c0
apachejuice <ubuntugeek1904@gmail.com>
2022-05-01 07:20:29 +0000

    update .gitinore, remove password requirement

--- internal/server/auth/account.go
+++ internal/server/auth/account.go
@@ -3,7 +3,6 @@ package auth
 import (
 	"fmt"
 	"strings"
-	"unicode"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -15,7 +14,7 @@ type Account struct {
 
 func NewAccount(username, password string) (Account, error) {
 	if len(username) < 5 || len(username) > 99 {
-		return Account{}, fmt.Errorf("username too long: must be 5-100 characters")
+		return Account{}, fmt.Errorf("username must be 5-100 characters")
 	}
 
 	err := pwCheckRequirements(password)
@@ -36,29 +35,8 @@ func pwCheckRequirements(pw string) error {
 		return fmt.Errorf("password cannot be empty or consist entirely of whitespace")
 	}
 
-	if len(pw) < 6 {
-		return fmt.Errorf("password must be longer than 6 characters")
-	}
-
-	hasUpper := false
-	hasLower := false
-	hasNumberOrCntrl := false
-	for _, c := range pw {
-		if unicode.IsUpper(c) {
-			hasUpper = true
-		}
-
-		if unicode.IsLower(c) {
-			hasLower = true
-		}
-
-		if unicode.IsNumber(c) || unicode.IsControl(c) {
-			hasNumberOrCntrl = true
-		}
-	}
-
-	if !(hasUpper && hasLower && hasNumberOrCntrl) {
-		return fmt.Errorf("password must contain all of: upper case letter, lower case letter, number or control characters")
+	if len(pw) < 8 {
+		return fmt.Errorf("password must be longer than 8 characters")
 	}
 
 	return nil
